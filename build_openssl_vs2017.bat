36730e457bd8d657889e12f5c3b231112bcbeb5b
Jun Zeng <junzeng@qti.qualcomm.com>
2018-04-27 13:38:18 +0800

    make it compatible with OpenSSL_1_1_0-stable branch updated

--- build_openssl_vs2017.bat
+++ build_openssl_vs2017.bat
@@ -9,48 +9,59 @@ cd /d %CWD%
 :: path ::
 set path="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.12.25827\bin\Hostx86\x86";"C:\Program Files (x86)\NASM";"C:\Perl64\bin";%path%
 
-
 perl Configure --release VC-WIN32 no-dso no-shared threads
-perl -pi.bak -e "s/ -DUNICODE -D_UNICODE/ /gi" configdata.pm
 perl -pi.bak -e "s/\/MT/\/MTd/gi" configdata.pm
 perl -pi.bak -e "s/\/O2/\/Od \/Ob0/gi" configdata.pm
-perl -pi.bak -e "s/ -D\"NDEBUG\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ -D\"UNICODE\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ -D\"_UNICODE\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ \"UNICODE\",/ /gi" configdata.pm
-perl -pi.bak -e "s/ \"_UNICODE\",/ /gi" configdata.pm
+perl -pi.bak -e "s/-D\"NDEBUG\"//gi" configdata.pm
+perl -pi.bak -e "s/\"NDEBUG\",//gi" configdata.pm
+perl -pi.bak -e "s/-DNDEBUG//gi" configdata.pm
+perl -pi.bak -e "s/-DUNICODE//gi" configdata.pm
+perl -pi.bak -e "s/-D_UNICODE//gi" configdata.pm
+perl -pi.bak -e "s/-D\"UNICODE\"//gi" configdata.pm
+perl -pi.bak -e "s/-D\"_UNICODE\"//gi" configdata.pm
+perl -pi.bak -e "s/\"UNICODE\",//gi" configdata.pm
+perl -pi.bak -e "s/\"_UNICODE\",//gi" configdata.pm
 del /F /Q "configdata.pm.bak" > nul
-perl -pi.bak -e "s/ -DUNICODE -D_UNICODE/ /gi" makefile
 perl -pi.bak -e "s/\/MT/\/MTd/gi" makefile
 perl -pi.bak -e "s/\/O2/\/Od \/Ob0/gi" makefile
-perl -pi.bak -e "s/ -D\"NDEBUG\"/ /gi" makefile
-perl -pi.bak -e "s/ -D\"UNICODE\"/ /gi" makefile
-perl -pi.bak -e "s/ -D\"_UNICODE\"/ /gi" makefile
-perl -pi.bak -e "s/ \"UNICODE\",/ /gi" makefile
-perl -pi.bak -e "s/ \"_UNICODE\",/ /gi" makefile
+perl -pi.bak -e "s/-D\"NDEBUG\"//gi" makefile
+perl -pi.bak -e "s/\"NDEBUG\",//gi" makefile
+perl -pi.bak -e "s/-DNDEBUG//gi" makefile
+perl -pi.bak -e "s/-DUNICODE//gi" makefile
+perl -pi.bak -e "s/-D_UNICODE//gi" makefile
+perl -pi.bak -e "s/-D\"UNICODE\"//gi" makefile
+perl -pi.bak -e "s/-D\"_UNICODE\"//gi" makefile
+perl -pi.bak -e "s/\"UNICODE\",//gi" makefile
+perl -pi.bak -e "s/\"_UNICODE\",//gi" makefile
 del /F /Q "makefile.bak" > nul
 
 nmake libclean
 nmake clean
 nmake distclean
 perl Configure --release VC-WIN32 no-dso no-shared threads
-perl -pi.bak -e "s/ -DUNICODE -D_UNICODE/ /gi" configdata.pm
 perl -pi.bak -e "s/\/MT/\/MTd/gi" configdata.pm
 perl -pi.bak -e "s/\/O2/\/Od \/Ob0/gi" configdata.pm
-perl -pi.bak -e "s/ -D\"NDEBUG\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ -D\"UNICODE\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ -D\"_UNICODE\"/ /gi" configdata.pm
-perl -pi.bak -e "s/ \"UNICODE\",/ /gi" configdata.pm
-perl -pi.bak -e "s/ \"_UNICODE\",/ /gi" configdata.pm
+perl -pi.bak -e "s/-D\"NDEBUG\"//gi" configdata.pm
+perl -pi.bak -e "s/\"NDEBUG\",//gi" configdata.pm
+perl -pi.bak -e "s/-DNDEBUG//gi" configdata.pm
+perl -pi.bak -e "s/-DUNICODE//gi" configdata.pm
+perl -pi.bak -e "s/-D_UNICODE//gi" configdata.pm
+perl -pi.bak -e "s/-D\"UNICODE\"//gi" configdata.pm
+perl -pi.bak -e "s/-D\"_UNICODE\"//gi" configdata.pm
+perl -pi.bak -e "s/\"UNICODE\",//gi" configdata.pm
+perl -pi.bak -e "s/\"_UNICODE\",//gi" configdata.pm
 del /F /Q "configdata.pm.bak" > nul
-perl -pi.bak -e "s/ -DUNICODE -D_UNICODE/ /gi" makefile
 perl -pi.bak -e "s/\/MT/\/MTd/gi" makefile
 perl -pi.bak -e "s/\/O2/\/Od \/Ob0/gi" makefile
-perl -pi.bak -e "s/ -D\"NDEBUG\"/ /gi" makefile
-perl -pi.bak -e "s/ -D\"UNICODE\"/ /gi" makefile
-perl -pi.bak -e "s/ -D\"_UNICODE\"/ /gi" makefile
-perl -pi.bak -e "s/ \"UNICODE\",/ /gi" makefile
-perl -pi.bak -e "s/ \"_UNICODE\",/ /gi" makefile
+perl -pi.bak -e "s/-D\"NDEBUG\"//gi" makefile
+perl -pi.bak -e "s/\"NDEBUG\",//gi" makefile
+perl -pi.bak -e "s/-DNDEBUG//gi" makefile
+perl -pi.bak -e "s/-DUNICODE//gi" makefile
+perl -pi.bak -e "s/-D_UNICODE//gi" makefile
+perl -pi.bak -e "s/-D\"UNICODE\"//gi" makefile
+perl -pi.bak -e "s/-D\"_UNICODE\"//gi" makefile
+perl -pi.bak -e "s/\"UNICODE\",//gi" makefile
+perl -pi.bak -e "s/\"_UNICODE\",//gi" makefile
 
 del /F /Q "makefile.bak" > nul
 
